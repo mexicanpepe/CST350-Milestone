f6d965c66d26d78363db7ab4f5155156de4e958c
Joseph Camarena <pepecamarena5@gmail.com>
2024-08-24 10:43:25 -0700

    Created registration form. utilizing SecurityDAO that is connected to local mySQL DB, Registration works. Failure if user email already exists in DB and successfull if user does not already exist

---
+++ Services/SecurityDAO.cs
@@ -0,0 +1,118 @@
+﻿using MySql.Data.MySqlClient;
+using CST350_Minesweeper.Models; 
+
+namespace CST350_Minesweeper.Services
+{
+    public class SecurityDAO
+    {
+        private string connectionString = "Server=192.168.0.214;Port=3306;Database=MinesweeperDB;User=root;Password=Qaz123wsx!;CharSet=utf8;";
+
+        public bool FindUserByNameAndPassword(User user)
+        {
+            //assume no user if found
+            bool success = false;
+
+            string sqlStatement = "SELECT * FROM Users WHERE Username = @username AND Password = @password";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@username", user.Username);
+                command.Parameters.AddWithValue("@password", user.Password);
+
+                try
+                {
+                    connection.Open();
+                    MySqlDataReader reader = command.ExecuteReader();
+                    success = reader.HasRows;
+                    reader.Close();
+                }
+
+                //errror handling
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+
+
+
+            return success;
+        }
+
+        public bool isCurrentUser(string email)
+        {
+
+            //assuming that the user does not exists in the DB
+            bool userExists = false;
+
+            //checking to see if the user input email exists
+            string sqlStatement = "SELECT * FROM Users WHERE Email = @Email";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(sqlStatement, connection);
+
+                command.Parameters.AddWithValue("@Email", email.ToLower());
+
+                try
+                {
+                    connection.Open();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    userExists = reader.HasRows;
+
+                    reader.Close();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+
+
+
+            return userExists;
+        }
+
+
+        //this method will add a user to the database based on registration form input
+        //will be invoked upon successfull registration
+        public void AddUser(User user)
+        {
+            string sqlQuery = "INSERT INTO Users (FirstName, LastName, Sex, Age, State, Email, Username, Password) " +
+                                  "VALUES (@FirstName, @LastName, @Sex, @Age, @State, @Email, @Username, @Password)";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+
+                MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+
+                //parameters for what is being inserted into User table
+                command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                command.Parameters.AddWithValue("@LastName", user.LastName);
+                command.Parameters.AddWithValue("@Sex", user.Sex);
+                command.Parameters.AddWithValue("@Age", user.Age);
+                command.Parameters.AddWithValue("@State", user.State);
+                command.Parameters.AddWithValue("@Email", user.Email.ToLower());
+                command.Parameters.AddWithValue("@Username", user.Username);
+                command.Parameters.AddWithValue("@Password", user.Password);
+
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("New user added to the database successfully.");
+                }
+
+                //error handling message
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+        }
+    }
+}
