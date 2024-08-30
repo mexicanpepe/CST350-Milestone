b957cbd147805ecd4761855794dc9bdb73330c77
Joseph Camarena <pepecamarena5@gmail.com>
2024-08-30 15:20:15 -0700

    added a home controller that will display a Home.cshtml and update to program.cs to make the Home Index the default view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CST350_Minesweeper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
